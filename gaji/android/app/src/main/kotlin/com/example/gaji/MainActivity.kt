1ef2d86dda1e994d2f1fe992caa44633f6ee39ac
just4funway <s32210077@student.ubm.ac.id>
2023-09-29 10:35:17 +0700

    First Commit

---
+++ gaji/android/app/src/main/kotlin/com/example/gaji/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gaji
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ kindoy/android/app/src/main/kotlin/com/example/kindoy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kindoy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
